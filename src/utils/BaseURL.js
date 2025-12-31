869e5d00525ad60498e373f204e321aa690fff94
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2025-12-31 17:08:31 +0600

    live url

--- src/utils/BaseURL.js
+++ src/utils/BaseURL.js
@@ -1,3 +1,4 @@
 // export const baseURL = "http://10.10.7.103:9002";
 
-export const baseURL = "https://ismail4001.binarybards.online";
+// export const baseURL = "https://ismail4001.binarybards.online";
+export const baseURL = "http://204.197.173.144:4001";
